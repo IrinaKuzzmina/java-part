b0b57340ef8b859d87e8be5f77d933f829f0df2b
IrinaKuzzmina <kuzmina.irina.acc@gmail.com>
2018-10-29 12:03:24 -0400

    Tasks 1.d and 1.e are added to the Homework09

---
+++ src/com/academy/telesens/HomeWork09/TimeImplSeconds.java
@@ -0,0 +1,17 @@
+package com.academy.telesens.HomeWork09;
+
+public class TimeImplSeconds extends TimeImpl implements Time {
+    int seconds;
+
+    @Override
+    public void setTime(int hours, int minutes, int seconds) {
+        super.setTime(hours, minutes, seconds);
+
+    }
+
+    @Override
+    public String getTime(TimeFormat timeFormat) {
+        return super.getTime(timeFormat);
+
+    }
+}
--- src/com/academy/telesens/HomeWork09/TimeTest.java
+++ src/com/academy/telesens/HomeWork09/TimeTest.java
@@ -7,8 +7,20 @@ import static com.academy.telesens.HomeWork09.TimeFormat.TIME_FORMAT_24;
 public class TimeTest {
     public static void main(String[] args) {
         TimeImpl time = new TimeImpl();
-        time.setTime(13,23,21);
-       System.out.println(time.getTime(TIME_FORMAT_24));
-       System.out.println(time.getTime(TIME_FORMAT_12));
+        time.setTime(13, 23, 21);
+        System.out.println(time.getTime(TIME_FORMAT_24));
+        System.out.println(time.getTime(TIME_FORMAT_12));
+
+        Time timeSimple = new TimeImpl();
+        timeSimple.setTime(12, 14, 33);
+        System.out.println(timeSimple.getTime(TIME_FORMAT_24));
+        System.out.println(timeSimple.getTime(TIME_FORMAT_12));
+
+        Time timeSeconds = new TimeImplSeconds();
+        timeSeconds.setTime(14, 17, 30);
+        System.out.println(timeSeconds.getTime(TIME_FORMAT_24));
+        System.out.println(timeSeconds.getTime(TIME_FORMAT_12));
+
+
     }
 }
